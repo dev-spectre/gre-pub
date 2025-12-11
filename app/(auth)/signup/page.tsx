74a0a07fce4d6885761e56ff10df59bc397f1803
dev-spectre <abhishekdallasalpy@gmail.com>
2025-12-11 18:46:21 +0530

    Update page min height

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -2,7 +2,7 @@ import { SignUpForm } from "@/components/Forms";
 
 export default function SignUpPage() {
   return (
-    <div className="px-fluid-container flex min-h-screen items-center justify-center bg-gradient-to-b from-[#e7ecf4] to-[#bec9df] py-12">
+    <div className="px-fluid-container flex min-h-[100dvh] items-center justify-center bg-gradient-to-b from-[#e7ecf4] to-[#bec9df] py-12">
       <SignUpForm />
     </div>
   );
