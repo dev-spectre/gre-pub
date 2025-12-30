5ea8e280b26ea89e56978f39e9ad97392f30b33a
Kishan Tiwari <kishanmanitiwari@gmail.com>
2025-12-30 14:27:07 +0530

    Fixed Build Issues

--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react"; // 1. Add this import
 import { ResetPasswordForm } from "@/components/Forms";
 
 export default function ResetPasswordPage() {
   return (
     <div className="px-fluid-container flex min-h-[100dvh] items-center justify-center bg-gradient-to-b from-[#e7ecf4] to-[#bec9df] py-12">
-      <ResetPasswordForm />
+      <Suspense fallback={<div>Loading...</div>}>
+        <ResetPasswordForm />
+      </Suspense>
     </div>
   );
 }
