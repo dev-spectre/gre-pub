d27e9ed8cf55d54f88fa0672430db0bccbb43200
dev-spectre <abhishekdallasalpy@gmail.com>
2026-02-21 19:38:03 +0530

    Update middlware to redirect payment page if clicked on dashboard and user hasn't paid

--- proxy.ts
+++ proxy.ts
@@ -4,13 +4,12 @@ import { getToken } from "next-auth/jwt";
 export async function proxy(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
 
-  console.log(token);
   if (!token) {
     return NextResponse.redirect(new URL("/signin", req.url));
   }
 
   if (req.url.includes("/dashboard") && !token.hasPaid) {
-    return NextResponse.redirect(new URL("/", req.url));
+    return NextResponse.redirect(new URL(`https://pages.razorpay.com/pl_SIjHdXZ7u3cFsR/view?email=${token.email}`));
   }
 
   return NextResponse.next();
